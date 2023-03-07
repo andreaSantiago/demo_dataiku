e9e266094ee504dda6c1866f328db1aea3763a2b
admin <admin>
2023-03-07 16:25:10 +0100

    Saved web app Real-Time Fraud Scoring (INSURANCEFRAUDDETECTIONNEW.GLTawAf)

--- web_apps/GLTawAf/backend.py
+++ web_apps/GLTawAf/backend.py
@@ -49,4 +49,4 @@ def scoring_api_call(params):
     print(prediction)    
     #return prediction['probas'] 
     return json.dumps({"results": prediction})
-    print(results) 
+    #print(results) 
